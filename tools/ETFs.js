67f865c2b5c41507c4b305544fc2f3616809a626
Carlos Augusto <solrac@gmail.com>
2023-04-07 04:31:22 -0300

    Update to output to HTML

--- tools/ETFs.js
+++ tools/ETFs.js
@@ -1,20 +1,21 @@
 function filterJsonObjects(jsonObject) {
-	const substrings = ["5L", "5S", "3L", "3S"];
+  const substrings = ["5L", "5S", "3L", "3S"];
 
-	const filteredObjects = jsonObject.filter(obj => {
-		const currency = obj.currency;
+  const filteredObjects = jsonObject.filter(obj => {
+	const currency = obj.currency;
 	return substrings.some(substring => currency.includes(substring));
-	});
+  });
 
-	return filteredObjects;
+  return filteredObjects;
 }
 
 // Fetching the JSON data from the API endpoint
 fetch("https://api.gateio.ws/api/v4/spot/currencies")
-	.then(response => response.json())
-	.then(jsonObject => {
-		const filteredJsonObject = filterJsonObjects(jsonObject);
-		const flattenedCurrencyArray = filteredJsonObject.map(obj => obj.currency).flat();
-		console.log(flattenedCurrencyArray);
-	})
-	.catch(error => console.error(error));
+  .then(response => response.json())
+  .then(jsonObject => {
+	const filteredJsonObject = filterJsonObjects(jsonObject);
+	const flattenedCurrencyArray = filteredJsonObject.map(obj => obj.currency).flat();
+	const currencyList = flattenedCurrencyArray.join("<br>");
+	document.body.innerHTML = `<p>${currencyList}</p>`;
+  })
+  .catch(error => console.error(error));
