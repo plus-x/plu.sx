1910349cf3e326c09b4c2a4599ad8eedc090f240
Carlos Augusto <solrac@gmail.com>
2023-04-07 06:49:43 -0300

    AI fix

--- tools/ETFs/ETFs.js
+++ tools/ETFs/ETFs.js
@@ -1,13 +1,15 @@
 function filterJsonObjects(jsonObject, feed) {
   const substrings = ["5L", "5S", "3L", "3S"];
 
-  var filteredObjects = jsonObject.filter(obj => {
-	var currency;
-	if( feed == 1 ) currency = obj.currency;
-	if( feed == 2 ) currency = obj.symbol;
-	if( feed == 3 ) currency = obj.symbol;
-	return substrings.some(substring => currency.includes(substring));
-  });
+  var filteredObjects = jsonObject;
+  
+	if (feed === 1) {
+	  filteredObjects = filteredObjects.filter(obj => substrings.some(substring => obj.currency.includes(substring)));
+	} else if (feed === 2) {
+	  filteredObjects = filteredObjects.filter(obj => substrings.some(substring => obj.symbol.includes(substring)));
+	} else if (feed === 3) {
+	  filteredObjects = filteredObjects.filter(obj => substrings.some(substring => obj.symbol.includes(substring)));
+	}
 
   return filteredObjects;
 }
@@ -20,23 +22,23 @@ function filterJsonObjects(jsonObject, feed) {
 		box;
 		
 	// Fetching the JSON data from the API endpoint
-	// fetch("ETFs.php?feed=1")
-	//   .then(response => {
-	// 	if (!response.ok) {
-	// 	  return response.text().then(text => {
-	// 		throw new Error(`HTTP error! Status: ${response.status}. Response data: ${text}`);
-	// 	  });
-	// 	}
-	// 	return response.json();
-	//   })
-	//   .then(jsonObject => {
-	// 	filteredJsonObject = filterJsonObjects(jsonObject, 1);
-	// 	flattenedCurrencyArray = filteredJsonObject.map(obj => obj.currency).flat();
-	// 	currencyList = flattenedCurrencyArray.join("<br>");
-	// 	box = document.querySelector("#GateIO");
-	// 	box.innerHTML = currencyList;
-	//   })
-	//   .catch(error => console.error(error));
+	fetch("ETFs.php?feed=1")
+	  .then(response => {
+		if (!response.ok) {
+		  return response.text().then(text => {
+			throw new Error(`HTTP error! Status: ${response.status}. Response data: ${text}`);
+		  });
+		}
+		return response.json();
+	  })
+	  .then(jsonObject => {
+		filteredJsonObject = filterJsonObjects(jsonObject, 1);
+		flattenedCurrencyArray = filteredJsonObject.map(obj => obj.currency).flat();
+		currencyList = flattenedCurrencyArray.join("<br>");
+		box = document.querySelector("#GateIO");
+		box.innerHTML = currencyList;
+	  })
+	  .catch(error => console.error(error));
 	
 	fetch("ETFs.php?feed=2")
 	  .then(response => {
