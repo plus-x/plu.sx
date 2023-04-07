3ebbfbc50cc4a31fa7f54b4889cf056dc68f56c6
Carlos Augusto <solrac@gmail.com>
2023-04-07 06:54:03 -0300

    Damn
    more AI goodness

--- tools/ETFs/ETFs.js
+++ tools/ETFs/ETFs.js
@@ -21,58 +21,58 @@ function filterJsonObjects(jsonObject, feed) {
 		currencyList,
 		box;
 		
-	// Fetching the JSON data from the API endpoint
-	fetch("ETFs.php?feed=1")
-	  .then(response => {
-		if (!response.ok) {
-		  return response.text().then(text => {
-			throw new Error(`HTTP error! Status: ${response.status}. Response data: ${text}`);
-		  });
-		}
-		return response.json();
-	  })
-	  .then(jsonObject => {
-		filteredJsonObject = filterJsonObjects(jsonObject, 1);
-		flattenedCurrencyArray = filteredJsonObject.map(obj => obj.currency).flat();
-		currencyList = flattenedCurrencyArray.join("<br>");
-		box = document.querySelector("#GateIO");
-		box.innerHTML = currencyList;
-	  })
-	  .catch(error => console.error(error));
-	
-	fetch("ETFs.php?feed=2")
-	  .then(response => {
-		if (!response.ok) {
-		  return response.text().then(text => {
-			throw new Error(`HTTP error! Status: ${response.status}. Response data: ${text}`);
-		  });
-		}
-		return response.json();
-	  })
-	  .then(jsonObject => {
-		filteredJsonObject = filterJsonObjects(jsonObject, 2);
-		flattenedCurrencyArray = filteredJsonObject.map(obj => obj.currency).flat();
-		currencyList = flattenedCurrencyArray.join("<br>");
-		box = document.querySelector("#KuCoin");
-		box.innerHTML = currencyList;
-	  })
-	  .catch(error => console.error(error));
-	  
-	  fetch("ETFs.php?feed=3")
+	(function() {
+	  // Fetching the JSON data from the API endpoint
+	  fetch("ETFs.php?feed=1")
 		.then(response => {
 		  if (!response.ok) {
-			return response.text().then(text => {
-			  throw new Error(`HTTP error! Status: ${response.status}. Response data: ${text}`);
-			});
+			throw new Error(`HTTP error! Status: ${response.status}. Response data: ${response.text()}`);
 		  }
 		  return response.json();
 		})
 		.then(jsonObject => {
-		  filteredJsonObject = filterJsonObjects(jsonObject, 3);
-		  flattenedCurrencyArray = filteredJsonObject.map(obj => obj.currency).flat();
-		  currencyList = flattenedCurrencyArray.join("<br>");
-		  box = document.querySelector("#MEXC");
+		  const filteredJsonObject = filterJsonObjects(jsonObject, 1);
+		  const flattenedCurrencyArray = filteredJsonObject.map(obj => obj.currency).flat();
+		  const currencyList = flattenedCurrencyArray.join("<br>");
+		  const box = document.querySelector("#GateIO");
 		  box.innerHTML = currencyList;
 		})
 		.catch(error => console.error(error));
+	
+	  (function() {
+		fetch("ETFs.php?feed=2")
+		  .then(response => {
+			if (!response.ok) {
+			  throw new Error(`HTTP error! Status: ${response.status}. Response data: ${response.text()}`);
+			}
+			return response.json();
+		  })
+		  .then(jsonObject => {
+			const filteredJsonObject = filterJsonObjects(jsonObject, 2);
+			const flattenedCurrencyArray = filteredJsonObject.map(obj => obj.symbol).flat();
+			const currencyList = flattenedCurrencyArray.join("<br>");
+			const box = document.querySelector("#KuCoin");
+			box.innerHTML = currencyList;
+		  })
+		  .catch(error => console.error(error));
+	  })();
+	
+	  (function() {
+		fetch("ETFs.php?feed=3")
+		  .then(response => {
+			if (!response.ok) {
+			  throw new Error(`HTTP error! Status: ${response.status}. Response data: ${response.text()}`);
+			}
+			return response.json();
+		  })
+		  .then(jsonObject => {
+			const filteredJsonObject = filterJsonObjects(jsonObject, 3);
+			const flattenedCurrencyArray = filteredJsonObject.map(obj => obj.name).flat();
+			const currencyList = flattenedCurrencyArray.join("<br>");
+			const box = document.querySelector("#MEXC");
+			box.innerHTML = currencyList;
+		  })
+		  .catch(error => console.error(error));
+	  })();
+	})();
 })();
